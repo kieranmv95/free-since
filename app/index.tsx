59da15e958f1cf37bc5e681e63fd0a85ed68b87b
kieranvenison <venison@hey.com>
2024-04-23 16:24:08 +0100

    refactor: Used new DS inside the app home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, P } from '../modules/design-system';
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <Container>
+      <P>Open up App.tsx to start working on your app!</P>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
